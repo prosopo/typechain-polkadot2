b79af6c28346e9dd35fff259ca2cfab195e2ceb9
George Oastler <goastler4@gmail.com>
2024-06-26 16:38:12 +0100

    lint

--- packages/typechain-polkadot/src/utils/json.ts
+++ packages/typechain-polkadot/src/utils/json.ts
@@ -3,6 +3,6 @@
  * @param json - JSON string
  */
 export function minimizeJson(json: string): string {
-    // remove whitespace as it causes issues when parsing JSON
-	return JSON.stringify(JSON.parse(json)).replace(/\\n/g, '').replace(/\\r/g, '').replace(/\\t/g, '')
+	// remove whitespace as it causes issues when parsing JSON
+	return JSON.stringify(JSON.parse(json)).replace(/\\n/g, '').replace(/\\r/g, '').replace(/\\t/g, '');
 }
